e71d8e08d2ec2cda3d8fb373e0897d3c6a2011db
brianedwin <brian.edwin28@gmail.com>
2025-02-15 11:20:38 +0000

    Create server.js
    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoutes = require("./routes/auth");
+const protectedRoutes = require("./routes/protected");
+
+require("dotenv").config();
+require("./config/passport");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
+
+app.use("/auth", authRoutes);
+app.use("/protected", protectedRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
