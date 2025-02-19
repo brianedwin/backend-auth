6697b24510db25170f8589ea907b691b2cd58911
brianedwin <brian.edwin28@gmail.com>
2025-02-19 13:36:33 +0000

    Update server.js
    update

--- server.js
+++ server.js
@@ -17,4 +17,7 @@ app.use("/auth", authRoutes);
 app.use("/protected", protectedRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const HOST = "0.0.0.0";  // Allows external access
+
+app.listen(PORT, HOST, () => console.log(`Server running on http://${HOST}:${PORT}`));
+
