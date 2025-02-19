404effc72f4aaa1cedc1f54df385957dac387858
brianedwin <brian.edwin28@gmail.com>
2025-02-19 18:14:15 +0000

    Update passport.js
    update for input random password into users table

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const db = require("./db");
+const crypto = require("crypto");
 
 passport.use(
     new GoogleStrategy(
@@ -14,10 +15,13 @@ passport.use(
                 let user = await db.query("SELECT * FROM users WHERE email = $1", [profile.emails[0].value]);
 
                 if (!user.rows.length) {
-                    // Create new user if not exists
+                    // Generate a random password (Google users don't need it)
+                    const randomPassword = crypto.randomBytes(16).toString("hex");
+
+                    // Insert user with a random password
                     user = await db.query(
                         "INSERT INTO users (email, password, role, is_verified) VALUES ($1, $2, $3, TRUE) RETURNING *",
-                        [profile.emails[0].value, null, "student"]
+                        [profile.emails[0].value, randomPassword, "student"]
                     );
                 }
 
